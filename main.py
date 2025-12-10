336aa62978bf1e472562e7b30e31cbb0a95b6a81
Kabeer <kabeermarch24@gmail.com>
2025-12-11 03:17:56 +0530

    Day 2: Added grayscale and Gaussian blur preprocessing

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # 0 = default webcam
+cap = cv2.VideoCapture(0)# Open the default camera
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
+    ret, frame = cap.read()# Read a frame from the camera
+    if not ret:#
         break
-    
-    cv2.imshow("Webcam Test", frame)
-    
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)# Convert the frame to grayscale
+    blur = cv2.GaussianBlur(gray, (21, 21), 0)# Apply Gaussian blur to the grayscale frame
+
+    cv2.imshow("Color", frame)# Display the original frame
+    cv2.imshow("Gray", gray)# Display the grayscale frame
+    cv2.imshow("Blur", blur)# Display the blurred frame
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
 
 cap.release()
